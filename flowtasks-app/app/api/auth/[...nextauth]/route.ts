d3e1b0911357386c1960a99c98d8a2523efc712d
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2026-04-08 09:57:14 -0300

    modified:   flowtasks-app/app/api/auth/[...nextauth]/route.ts

--- flowtasks-app/app/api/auth/[...nextauth]/route.ts
+++ flowtasks-app/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
 // app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@/auth"; // Importa do arquivo que você criou no Passo 1
+import { handlers } from "@/auth"; 
 export const { GET, POST } = handlers;
